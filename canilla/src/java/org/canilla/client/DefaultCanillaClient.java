bc076d832f8827a65085a8d5da8c4b0954b77a70
dromoli <dromoli>
2004-10-07 13:28:24 +0000

    updating...

--- canilla/src/java/org/canilla/client/DefaultCanillaClient.java
+++ canilla/src/java/org/canilla/client/DefaultCanillaClient.java
@@ -1,8 +1,10 @@
 package org.canilla.client;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.logging.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * @author dromoli
@@ -11,10 +13,9 @@ public class DefaultCanillaClient implements CanillaClient {
 
     private Log log = LogFactory.getLog(DefaultCanillaClient.class);
     // the newsfeeds this client is going to suck news from
-    private List newsfeeds;
+    private List newsfeeds = new ArrayList();
     
     private DefaultCanillaClient() {
-        
     }
     
     public CanillaClient getInstance() {
